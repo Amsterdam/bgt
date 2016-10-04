f89d63afc31a154b4e0214a4a25bd35ca07e32df
young001 <r.young@amsterdam.nl>
2016-10-04 13:35:07 +0200

    Datapunt/BGT: 1.00.0, RC1: 090_aanmaak_views: hernoemen (veralgemeniseren) viewnamen. RY, 04-10-2016.

---
+++ app/090_aanmaak_VIEWS_BGT/create_vw_bgt_ondersteunendwaterdeel.sql
@@ -0,0 +1,54 @@
+﻿
+\qecho
+\qecho '*******************************************************************************'
+\qecho '* Aanmaak view imgeo_extractie.vw_bgt_ondersteunendwaterdeel ...              *'
+\qecho '*******************************************************************************'
+\qecho
+
+
+-- Schema: imgeo_extractie
+
+DROP VIEW IF EXISTS imgeo_extractie.vw_bgt_ondersteunendwaterdeel;
+
+CREATE OR REPLACE VIEW imgeo_extractie.vw_bgt_ondersteunendwaterdeel
+AS
+(
+SELECT identificatie_namespace     as NAMESPACE
+     , identificatie_lokaalid      as LOKAALID
+     , objectbegintijd             as BEGINTIJD
+     , objecteindtijd              as EINDDTIJD
+     , tijdstipregistratie         as TIJDREG     
+     , eindregistratie             as EINDREG
+     , lv_publicatiedatum          as LV_PUBDAT
+     , bronhouder                  as BRONHOUD
+     , inonderzoek                 as INONDERZK
+     , relatievehoogteligging      as HOOGTELIG
+     , bgt_status                  as BGTSTATUS
+     , plus_status                 as PLUSSTATUS
+     , bgt_type                    as BGTTYPE
+     , 'BGT_OWDL_'||
+        LOWER( REPLACE(                   
+                    REPLACE(
+                        Replace(
+                            REPLACE(
+                                REPLACE(
+                                    REPLACE(
+                                        REPLACE (bgt_type ,',',''),  -- vervangen ',' tekens met niks  
+                                    '/ ',''), -- vervangen '/spatie' tekens met niks 
+                                ':',''), -- vervangen ':' tekens met niks 
+                            '(',''), -- vervangen '(' tekens met niks 
+                        ')',''), -- vervangen ')' tekens met niks         
+                    '/','_'), -- vervangen '/' tekens met underscore (tbv en/of -> en_of)             
+                ' ','_') -- vervangen 'spatie' tekens met '_'     
+              )                    as BESTANDSNAAM
+     , plus_type                   as PLUSTYPE
+     , geometrie                   as GEOMETRIE
+  FROM imgeo.bgt_ondersteunendwaterdeel)
+  ;
+
+
+\qecho
+\qecho '*******************************************************************************'
+\qecho '* Klaar met aanmaak view imgeo_extractie.vw_bgt_ondersteunendwaterdeel.       *'
+\qecho '*******************************************************************************'
+\qecho
---
+++ app/090_aanmaak_VIEWS_BGT/create_vw_imgeo_scheiding.sql
@@ -0,0 +1,82 @@
+﻿
+\qecho
+\qecho '*******************************************************************************'
+\qecho '* Aanmaak view imgeo_extractie.vw_imgeo_scheiding ...                         *'
+\qecho '*******************************************************************************'
+\qecho
+
+
+-- Schema: imgeo_extractie
+
+DROP VIEW IF EXISTS imgeo_extractie.vw_imgeo_scheiding;
+
+CREATE OR REPLACE VIEW imgeo_extractie.vw_imgeo_scheiding
+AS
+(
+SELECT identificatie_namespace     as NAMESPACE
+     , identificatie_lokaalid      as LOKAALID
+     , objectbegintijd             as BEGINTIJD
+     , objecteindtijd              as EINDDTIJD
+     , tijdstipregistratie         as TIJDREG     
+     , eindregistratie             as EINDREG
+     , lv_publicatiedatum          as LV_PUBDAT
+     , bronhouder                  as BRONHOUD
+     , inonderzoek                 as INONDERZK
+     , relatievehoogteligging      as HOOGTELIG
+     , bgt_status                  as BGTSTATUS
+     , plus_status                 as PLUSSTATUS
+     , bgt_type                    as BGTTYPE    
+     , case
+        when bgt_type ='niet-bgt' -- cntr op bgt_type ='niet-bgt'
+     then null
+     else
+          'BGT_SDG_'|| -- prefixed toevoegen aan BESTANDSNAAM
+    LOWER(        REPLACE(                   
+                REPLACE(
+                    Replace(
+                        REPLACE(
+                            REPLACE(
+                                REPLACE(
+                                    REPLACE (bgt_type ,',',''),  -- vervangen ',' tekens met niks  
+                                '/ ',''), -- vervangen '/spatie' tekens met niks 
+                            ':',''), -- vervangen ':' tekens met niks 
+                        '(',''), -- vervangen '(' tekens met niks 
+                    ')',''), -- vervangen ')' tekens met niks         
+                '/','_'), -- vervangen '/' tekens met underscore (tbv en/of -> en_of)             
+            ' ','_') -- vervangen 'spatie' tekens met '_' teken         
+             )
+    end                            as BESTANDSNAAM
+     , plus_type                   as PLUSTYPE 
+     ,case
+    when bgt_type ='niet-bgt' and plus_type is null -- cntr op bgt_type ='niet-bgt' en plus_type is leeg
+         then 'BGTPLUS_SDG_'||'onbekend' --prefixed en plustype 'onbekend' in BESTANDSNAAM_plus
+         else
+            case
+            when bgt_type ='niet-bgt' -- cntr op bgt_type ='niet-bgt' en plustype is wel gevuld
+            then
+                 'BGTPLUS_SDG_'|| --prefixed en plustype in BESTANDSNAAM_plus
+        LOWER(        REPLACE(                   
+                    REPLACE(
+                        Replace(
+                            REPLACE(
+                                REPLACE(
+                                    REPLACE(
+                                        REPLACE (plus_type ,',',''),  -- vervangen ',' tekens met niks  
+                                    '/ ',''), -- vervangen '/spatie' tekens met niks 
+                                ':',''), -- vervangen ':' tekens met niks 
+                            '(',''), -- vervangen '(' tekens met niks 
+                        ')',''), -- vervangen ')' tekens met niks         
+                    '/','_'), -- vervangen '/' tekens met underscore (tbv en/of -> en_of)             
+                ' ','_') -- vervangen 'spatie' tekens met '_'     
+            )end
+    end                            as BESTANDSNAAM_PLUS
+     , geometrie                   as GEOMETRIE
+  FROM imgeo.imgeo_scheiding)
+  ;
+
+
+\qecho
+\qecho '*******************************************************************************'
+\qecho '* Klaar met aanmaak view imgeo_extractie.vw_imgeo_scheiding.                  *'
+\qecho '*******************************************************************************'
+\qecho
