52e031173c586f7f72ec1371f804ed8fc0b41722
Thijs Creemers <thijs.creemers@gmail.com>
2016-11-07 15:17:59 +0100

    cleanup commeted out code

--- app/import_bgt.sh
+++ app/import_bgt.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
-#cat > ~/.pgpass <<EOF
-#database:5432:bgt:bgt:insecure
-#${FMEHOST}:5432:gisdb:dbuser:${FMEDBPASS}
-#EOF
-#
-#chmod 600 ~/.pgpass
-#
-#mkdir log
-#
-#cd /app/
 /usr/bin/python3 /src/fme/connect.py
