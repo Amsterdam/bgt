3cda7c5cd97fb2f7156d773f4d95d0e11555259f
Thijs Creemers <thijs.creemers@gmail.com>
2016-11-14 11:21:08 +0100

    rename tellingen.py to comparison.py

--- src/fme/core.py
+++ src/fme/core.py
@@ -8,7 +8,7 @@ from zipfile import ZipFile
 from objectstore.objectstore import ObjectStore
 import requests
 from fme.sql_utils import run_sql_script, import_gml_control_db, import_csv_fixture
-from fme.tellingen import compare_before_after_counts_csv
+from fme.comparison import compare_before_after_counts_csv
 from setup import FME_SERVER_API, FME_SERVER, INSTANCE_ID, SCRIPT_ROOT
 
 from fme import fme_server
