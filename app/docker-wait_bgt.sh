706deb481d9068fdde1b80d56b744cea6d851d14
dick <dick.kniep@lindix.nl>
2016-12-07 15:10:12 +0100

    Aanpassingen op basis van aangeven van Stef

--- app/docker-wait_bgt.sh
+++ app/docker-wait_bgt.sh
@@ -3,13 +3,6 @@
 set -u
 set -e
 
-# wait for postgres TMP
-while ! nc -z ${DATABASE_FME_1_PORT_5432_TCP_ADDR} ${DATABASE_FME_1_PORT_5432_TCP_PORT}
-do
-	echo "Waiting for postgres..."
-	sleep 1
-done
-
 # wait for postgres BGT
 while ! nc -z ${DATABASE_BGT_1_PORT_5432_TCP_ADDR} ${DATABASE_BGT_1_PORT_5432_TCP_PORT}
 do
