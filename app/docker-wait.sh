6cac7f49b448dcc76344b8754c0712727e188370
Thijs Creemers <thijs.creemers@gmail.com>
2017-02-22 15:27:07 +0100

    updated docker wait

--- app/docker-wait.sh
+++ app/docker-wait.sh
@@ -3,9 +3,9 @@
 set -u
 set -e
 
-# wait for postgres FME
-while ! nc -z ${DATABASE_PORT_5432_TCP_ADDR} ${DATABASE_PORT_5432_TCP_PORT}
+# wait for postgres
+while ! nc -z database 5432
 do
 	echo "Waiting for postgres..."
-	sleep 1
+	sleep 2
 done
