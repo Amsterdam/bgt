a8bd0cac96c1430e0d4a5db83f4e3f5b5dcb1205
Thijs Creemers <thijs.creemers@gmail.com>
2017-05-16 15:36:16 +0200

    remove param from transform dfgn

--- src/fme/transform_dgn.py
+++ src/fme/transform_dgn.py
@@ -28,7 +28,6 @@ def start_transformation_dgn(min_x, min_y, max_x, max_y):
          "NMDirectives": {"successTopics": [], "failureTopics": []},
          "TMDirectives": {"tag": "linux", "description": "Aanmaak NLCS uit DB"},
          "publishedParameters": [
-             {"name": "SourceDataset_POSTGIS", "value": "bgt"},
              {"name": "SourceDataset_POSTGIS_5", "value": "bgt"},
              {"name": "P_OUTPUT_DGN", "value": "$(FME_SHAREDRESOURCE_DATA)DGNv8"},
              {"name": "P_CEL", "value": ["$(FME_SHAREDRESOURCE_DATA)resources/NLCS.cel"]},
