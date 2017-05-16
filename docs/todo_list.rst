6b7be2ea0157acbb4d6a0d8fea3618a746704624
Pieter van Beek <pieter@djinnit.com>
2017-05-16 17:24:17 +0200

    Improved documentation.

--- docs/todo_list.rst
+++ docs/todo_list.rst
@@ -3,5 +3,3 @@
 Todo list
 =========
 
-.. todolist::
-
--- src/bgt_setup.py
+++ src/bgt_setup.py
@@ -1,4 +1,4 @@
-import os.path
+import os
 
 DEBUG = os.getenv('DEBUG', False) == '1'
 SCRIPT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app'))
@@ -17,4 +17,3 @@ DB_FME_DBNAME = os.getenv('DATABASE_ENV_POSTGRES_DB', 'gisdb')
 DB_FME_PW = os.getenv('DATABASE_ENV_POSTGRES_PASSWORD', 'insecure')
 DB_FME_PORT = os.getenv('DATABASE_PORT_5432_TCP_PORT', '5401')
 DB_FME_USER = os.getenv('BGT_DATABASE_ENV_POSTGRES_USER', 'dbuser')
-
