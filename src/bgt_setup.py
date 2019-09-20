eeacd39fcffd11b07d2a5e986d73761301a3bf88
Ron van Barneveld <r.van.barneveld@amsterdam.nl>
2019-09-20 15:05:22 +0200

    database waarde aangeapst naar "bgt" omdat dit de database is die FMEcloud benaderd.

--- src/bgt_setup.py
+++ src/bgt_setup.py
@@ -13,7 +13,7 @@ INSTANCE_ID = os.getenv('FMEINSTANCE', 'secret')
 FME_DBPASS = os.getenv('FMEDBPASS', 'secret')
 
 DB_FME_HOST = os.getenv('DATABASE_PORT_5432_TCP_ADDR', 'localhost')
-DB_FME_DBNAME = os.getenv('DATABASE_ENV_POSTGRES_DB', 'gisdb')
+DB_FME_DBNAME = os.getenv('DATABASE_ENV_POSTGRES_DB', 'bgt')
 DB_FME_PW = os.getenv('DATABASE_ENV_POSTGRES_PASSWORD', 'insecure')
 DB_FME_PORT = os.getenv('DATABASE_PORT_5432_TCP_PORT', '5401')
 DB_FME_USER = os.getenv('BGT_DATABASE_ENV_POSTGRES_USER', 'dbuser')
